6ffb5ea07ebf21bbafe81fcda64d20620fbf3521
stla <laurent_step@yahoo.fr>
2023-07-10 04:51:51 +0200

    addin

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# swipeR 1.0.0
+
+RStudio addin to choose and display image files in the viewer pane.
+
+
 # swipeR 0.1.0
 
 First release.
---
+++ R/addin.R
@@ -0,0 +1,27 @@
+#' @importFrom rChoiceDialogs getDefaultFilters rchoose.files
+#' @importFrom base64enc dataURI
+#' @importFrom tools file_ext
+#' @importFrom htmltools div img
+#' @noRd
+#' @keywords internal
+swipeRaddin <- function() {
+  ns <- asNamespace("rChoiceDialogs")
+  attachNamespace(ns)
+  filters <- rbind(
+    c("Image files", "*.png;*.bmp;*.jpeg;*.jpg;*.gif"),
+    c("GIF files (*.gif)", "*.gif"),
+    getDefaultFilters()[5L:7L, ]
+  )
+  paths <- rchoose.files(filters = filters, index = 1L)
+  divs <- vector("list", length = length(paths))
+  for(i in seq_along(paths)) {
+    image <- paths[i]
+    b64 <- dataURI(file = image, mime = paste0("image/", file_ext(image)))
+    divs[[i]] <- div(
+      class = "swiper-zoom-container",
+      img(src = b64, style = "height: 90vh; width: 90vw; margin: auto;")
+    )
+  }
+  wrapper <- do.call(swipeRwrapper, divs)
+  swipeR(wrapper, height = "90vh", navigationColor = "red", zoom = TRUE)
+}
